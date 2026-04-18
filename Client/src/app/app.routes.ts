214425c2fbd962a00601576f836375585c4e1405
Vlad <vladyuzevuts@gmail.com>
2026-04-18 19:35:05 +0300

    Add work with Users
    Now can find user and add him to friends or family

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { SelectBankComponent } from './pages/select-bank/select-bank';
 import { DashboardComponent } from './pages/dashboard/dashboard'; 
 import { CardsComponent } from './pages/cards/cards';
 import { CardDetailsComponent } from './pages/card-details/card-details';
+import { SearchUserComponent } from './pages/search-user/search-user';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: DashboardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'select-bank', component: SelectBankComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards', component: CardsComponent },
   { path: 'card-details/:id', component: CardDetailsComponent },
+  { path: 'search-user', component: SearchUserComponent },
   { path: '**', redirectTo: '' } 
 ];
